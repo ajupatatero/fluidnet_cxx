1ea2666f682dd337e2781d5e58d667c521d2ace7
Ekhi Ajuria <ajuek95@hotmail.com>
2019-10-07 19:20:06 +0200

    Cylinder and Plume Correct

--- pytorch/lib/fluid/__init__.py
+++ pytorch/lib/fluid/__init__.py
@@ -6,11 +6,11 @@ from .flags_to_occupancy import flagsToOccupancy
 from .velocity_divergence import velocityDivergence
 from .velocity_update import velocityUpdate
 from .velocity_update import velocityUpdate_Density
-from .source_terms import addBuoyancy, addGravity
+from .source_terms import addBuoyancy, addGravity, addBuoyancy_NewSourceTerm
 from .viscosity import addViscosity
 from .geometry_utils import createCylinder, createBox2D
 from .util import emptyDomain
-from .init_conditions import createPlumeBCs, createRayleighTaylorBCs
+from .init_conditions import createPlumeBCs, createRayleighTaylorBCs, createVKBCs
 from .cpp.advection import correctScalar, advectScalar, advectVelocity
 from .cpp.solve_linear_sys import solveLinearSystemJacobi
 from .cpp.solve_linear_sys import solveLinearSystemJacobi_Density
